a2a065b37230f15cf8c93f803c58ba35c455a470
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-09-22 22:00:30 -0500

    add some stuff from another project that will be useful here

---
+++ gcr/malloc_config.h
@@ -0,0 +1,3 @@
+﻿#pragma once
+#define USE_DL_PREFIX 1
+#define MSPACES 1
---
+++ gcr/opcodes.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <cstdint>
+
+enum class OpCode : uint8_t
+{
+    HALT = 0x00,
+    PUSH = 0x01,
+    POP = 0x02,
+    ADD = 0x03,
+    SUB = 0x04,
+    MUL = 0x05,
+    DIV = 0x06,
+    JUMP = 0x07,
+    CALL = 0x08,
+    RET = 0x09,
+    JZ = 0x0A,
+    JNZ = 0x0B,
+    CMP_EQ = 0x0C,
+    CMP_LT = 0x0D,
+    CMP_GT = 0x0E,
+    DUP = 0x0F,
+    CALL_HOST = 0x10,
+    ENTER = 0x11,
+    LOAD = 0x12,
+    STORE = 0x13,
+    ALLOC = 0x14,
+    FREE = 0x15,
+    LOAD_HEAP = 0x16,
+    STORE_HEAP = 0x17,
+    NOP = 0x18,
+};
