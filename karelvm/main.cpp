1841d0466f6a05000254666772f32e64e643adc6
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-09-23 16:31:40 -0700

    rename karelui-cpp to karelui, remove some files

--- karelvm/main.cpp
+++ karelvm/main.cpp
@@ -2,35 +2,38 @@
 #include <exception>
 #include <iostream>
 
+using namespace karel_cpp;
+
+static KarelUI* karelui = nullptr;
+static int ticks = 0;
+static Karel* karel;
+
+static bool Tick()
+{
+    karel->Paint(Color{ 255, 180, 80 });
+
+    if (ticks % 10 == 9)
+        karel->RotateRight();
+    else
+        karel->Move();
+
+    ticks++;
+    return true;
+}
+
 int main()
 {
+    //HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    //DWORD mode = 0;
+    //GetConsoleMode(handle, &mode);
+    //SetConsoleMode(handle, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+
     try
     {
-        KarelUI* window = nullptr;
-        int ticks = 0;
-
-        KarelUI ui(
-            10, 10,
-            [&]() -> bool
-            {
-                auto* karel = window->GetKarel();
-
-                karel->Paint(Color{ 255, 180, 80 });
-
-                if (ticks % 10 == 9)
-                    karel->RotateRight();
-                else
-                    karel->Move();
-
-                ++ticks;
-                return true;
-            },
-            20,  // Cell size in pixels
-            200  // Milliseconds between steps
-        );
-
-        window = &ui;
-        return ui.Show();
+        karelui = new KarelUI(
+            10, 10, Tick, 20, 200);
+        karel = karelui->GetKarel();
+        return karelui->Show();
     }
     catch (const std::exception& error)
     {
